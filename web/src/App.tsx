56ae79756e0962bbe216f5f08b75e0b327089a38
Quentin Lathaud <quentin.lathaud@groupeseloger.com>
2025-06-15 22:35:31 +0200

    reorganize code

---
+++ web/src/App.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "@/web/components/ui/toaster";
+import { Toaster as Sonner } from "@/web/components/ui/sonner";
+import { TooltipProvider } from "@/web/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages";
+import Association from "./pages/Association";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/association" element={<Association />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
---
+++ web/src/components/ui/use-toast.ts
@@ -0,0 +1,3 @@
+import { useToast, toast } from "@/web/hooks/use-toast";
+
+export { useToast, toast };
